164eb15ef5ffbfe20e63de9e95e4794ddbac1ce6
Shengyu Liu <usungu@gmail.com>
2021-12-12 04:41:58 +0800

    Spin animation testing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
+      <h2>Css Animation</h2>
+      <div className="main">
+        <div className="card">Spin</div>
+      </div>
     </div>
   );
 }
