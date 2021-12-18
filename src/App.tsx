e163e769ab33c54e477cead7c06bc44efb58d180
Shengyu Liu <usungu@gmail.com>
2021-12-18 23:56:54 +0800

    Add recharts packageg and demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
+import RechartsDemo from 'components/RechartsDemo';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Css Animation</h2>
-      <div className="main">
-        <div className="card">Spin</div>
-      </div>
+      <RechartsDemo></RechartsDemo>
     </div>
   );
 }
---
+++ src/components/AnimationDemo/AnimationDemo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AnimationDemo.css';
+
+export default function AnimationDemo() {
+  return (
+    <div>
+      <h2>Css Animation</h2>
+      <div className="main">
+        <div className="card">Spin</div>
+      </div>
+    </div>
+  );
+}
